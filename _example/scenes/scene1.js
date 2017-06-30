a124cab00cb4c673badbfed8c99e746c8db82e2f
Naugtur <naugtur@gmail.com>
2017-06-30 14:10:20 +0200

    fix renderer, run a full scene with movement

--- _example/scenes/scene1.js
+++ _example/scenes/scene1.js
@@ -3,16 +3,14 @@
 // scene should create and expose a default world or accept one
 var scene = IIIdage.Scene({
   library: {
-    //sound library definition
+    // sound library definition
   },
   world: IIIdage.World({
     settings
   }),
   things: [ // scene iterates all things and spawns them into the world. same can be done manually later on.
     dog({
-      x: 1,
-      y: 1,
-      z: 1
+      pos: [1, 1, 1]
     })
   ]
 }).load().run()
